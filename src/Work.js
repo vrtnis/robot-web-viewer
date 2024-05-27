47459649d1d19e7fcafeba3261cd5c7721517602
victg <123119434+statistron@users.noreply.github.com>
2024-05-26 18:00:18 -0700

    Disable gridHelper

--- src/Work.js
+++ src/Work.js
@@ -162,7 +162,8 @@ export const Work = () => {
           <LoadModel filepath={modelPath} />
         </Suspense>
         <OrbitControls />
-        <gridHelper args={[10, 10]} />
+        {/* <gridHelper args={[10, 10]} /> */}
+
         <axesHelper />
       </Canvas>
     </div>
